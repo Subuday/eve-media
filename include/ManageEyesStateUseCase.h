7d2fe2187926b41b9ac068c2ba75cbee521f6a37
Subuday <mr.gigant977@gmail.com>
2024-05-18 12:24:22 +0300

    Manage eyes states

--- include/ManageEyesStateUseCase.h
+++ include/ManageEyesStateUseCase.h
@@ -1,16 +1,31 @@
 #pragma once
 
 #include <EyesView.h>
+#include <EyesState.h>
+#include <mutex>
+
+using namespace std;
 
 class ManageEyesStateUseCase {
     private:
         EyesView& view;
+        
+        recursive_mutex mtx;
+        EyesState currentState;
+        EyesState pendingState;
+        int consecutiveBlinkCount = 0;
+
+        void updateState();
+        void setPendingState(EyesState state);
+        
+        EyesState randomTriggeringState();
+        EyesState nextState(EyesState currentState, EyesState pendingState);
     public:
         ManageEyesStateUseCase(EyesView& view);
         void openEyes();
         void blink();
         void think();
         void speak();
-        void trigger(int index = -1);
+        void trigger(int index);
         void closeEyes();
 };
--- src/ManageEyesStateUseCase.cpp
+++ src/ManageEyesStateUseCase.cpp
@@ -1,34 +1,102 @@
 #include <EyesState.h>
 #include <ManageEyesStateUseCase.h>
 
-ManageEyesStateUseCase::ManageEyesStateUseCase(EyesView& view) : view(view) {}
+ManageEyesStateUseCase::ManageEyesStateUseCase(EyesView& view) : view(view), currentState(EyesState::NONE), pendingState(EyesState::NONE) {
+    view.setOnAnimationEndCallback([this]() {
+        updateState();
+    });
+}
+
+void ManageEyesStateUseCase::updateState() {
+    EyesState nextState;
+    {
+        lock_guard<recursive_mutex> lock(mtx);
+
+        nextState = this->nextState(currentState, pendingState);
+        EyesState nextPendingState = EyesState::NONE;
+
+        if (nextState == EyesState::BLINKING) {
+            consecutiveBlinkCount++;
+            if (consecutiveBlinkCount >= 2) {
+                nextPendingState = randomTriggeringState();
+                consecutiveBlinkCount = 0;
+            }
+        } else {
+            consecutiveBlinkCount = 0;
+        }
+        
+        currentState = nextState;
+        pendingState = nextPendingState;
+    }
+    
+    // TODO: Post on main thread
+    view.updateState(nextState);
+}
+
+void ManageEyesStateUseCase::setPendingState(EyesState state) {
+    lock_guard<recursive_mutex> lock(mtx);
+    pendingState = state;
+}
+
+EyesState ManageEyesStateUseCase::randomTriggeringState() {
+    return static_cast<EyesState>(rand() % 3 + 5);
+}
+
+EyesState ManageEyesStateUseCase::nextState(EyesState currentState, EyesState pendingState) {
+    if (pendingState == EyesState::SMILING ||
+        pendingState == EyesState::DVD ||
+        pendingState == EyesState::JUMPING) {
+        if (currentState == EyesState::BLINKING) {
+            return pendingState;
+        } else {
+            pendingState == EyesState::NONE; // Ignore triggering state
+        }
+    }
+
+    if (pendingState == EyesState::NONE) {
+        if (currentState == EyesState::SPEAKING) {
+            return EyesState::SPEAKING;
+        } else if (currentState == EyesState::THINKING) {
+            return EyesState::THINKING;
+        } else if (currentState == EyesState::CLOSING) {
+            return EyesState::NONE;
+        } else {
+            return EyesState::BLINKING;
+        }
+    }
+
+    return pendingState;
+}
 
 void ManageEyesStateUseCase::openEyes() {
-    view.updateState(EyesState::OPENING);
+    setPendingState(EyesState::OPENING);
+    updateState();
 }
 
 void ManageEyesStateUseCase::blink() {
-    view.updateState(EyesState::BLINKING);
+    setPendingState(EyesState::BLINKING);
 }
 
 void ManageEyesStateUseCase::think() {
-    view.updateState(EyesState::THINKING);
+    setPendingState(EyesState::THINKING);
+    updateState();
 }
 
 void ManageEyesStateUseCase::speak() {
-    view.updateState(EyesState::SPEAKING);
+    setPendingState(EyesState::SPEAKING);
+    updateState();
 }
 
 void ManageEyesStateUseCase::trigger(int index) {
     switch (index) {
         case 0:
-            view.updateState(EyesState::SMILING);
+            setPendingState(EyesState::SMILING);
             break;
         case 1:
-            view.updateState(EyesState::DVD);
+            setPendingState(EyesState::DVD);
             break;
         case 2:
-            view.updateState(EyesState::JUMPING);
+            setPendingState(EyesState::JUMPING);
             break;
         default:
             break;
@@ -36,5 +104,6 @@ void ManageEyesStateUseCase::trigger(int index) {
 }
 
 void ManageEyesStateUseCase::closeEyes() {
-    view.updateState(EyesState::CLOSING);
+    setPendingState(EyesState::CLOSING);
+    updateState();
 }
