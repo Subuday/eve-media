ed019be446f2311c19c2b6c7b4087fddbbb42951
Subuday <mr.gigant977@gmail.com>
2024-05-18 12:30:32 +0300

    Add animation callbacks

--- include/EyesView.h
+++ include/EyesView.h
@@ -2,20 +2,31 @@
 
 #include <View.hpp>
 #include <EyesState.h>
+#include <Timer.h>
 #include <string>
+#include <functional>
 
 using namespace std;
 
 class EyesView: public View {
     private:
+        function<void()> onAnimationStartCallback;
+        function<void()> onAnimationEndCallback;
+
         int frameChannels = 3;
 
         EyesState state = EyesState::NONE;
 
         int frameIndex = 0;
 
-        void drawFrame(int width, int height, uint16_t* buffer, string animation, int frames);
+        int animationFrames(EyesState state);
+        void drawFrame(int width, int height, uint16_t* buffer, string animation);
+        void drawFrame(EyesState state, int width, int height, uint16_t* buffer);
     public:
-        void updateState(EyesState state);
         void draw(int width, int height, uint16_t* buffer) override;
+
+        void setOnAnimaitonStartCallback(function<void()> cb);
+        void setOnAnimationEndCallback(function<void()> cb);
+
+        void updateState(EyesState state);
 };
--- src/EyesView.cpp
+++ src/EyesView.cpp
@@ -2,13 +2,39 @@
 #include <stb_image.h>
 #include <EyesView.h>
 
-// Assert NONE state
-void EyesView::updateState(EyesState state) {
-    frameIndex = 0;
-    this->state = state;
+void EyesView::setOnAnimaitonStartCallback(function<void()> cb) {
+    onAnimationStartCallback = cb;
 }
 
-void EyesView::drawFrame(int width, int height, uint16_t* buffer, string animation, int frames) {
+void EyesView::setOnAnimationEndCallback(function<void()> cb) {
+    onAnimationEndCallback = cb;
+}
+
+int EyesView::animationFrames(EyesState state) {
+    switch (state) {
+        case EyesState::OPENING:
+            return 362;
+        case EyesState::BLINKING:
+            return 363;
+        case EyesState::THINKING:
+            return 398;
+        case EyesState::SPEAKING:
+            return 333;
+        case EyesState::SMILING:
+            return 333;
+        case EyesState::DVD:
+            return 363;
+        case EyesState::JUMPING:
+            return 363;
+        case EyesState::CLOSING:
+            return 363;
+        default:
+            // TODO: assert
+            return 0;
+    }
+}
+
+void EyesView::drawFrame(int width, int height, uint16_t* buffer, string animation) {
     // TODO: Handle error
     string framePath = "../lib/eve-lcd-driver/res/" + animation + "/frame_" + to_string(frameIndex) + ".png";
     unsigned char* data = stbi_load(framePath.c_str(), &width, &height, &frameChannels, 0);
@@ -30,45 +56,59 @@ void EyesView::drawFrame(int width, int height, uint16_t* buffer, string animati
     }
 
     stbi_image_free(data);
+}
 
-    if (frameIndex == frames - 1) {
-        // frameIndex = 0;
+void EyesView::drawFrame(EyesState statem, int width, int height, uint16_t* buffer) {
+    switch (state) {
+        case EyesState::NONE:
+            return;
+        case EyesState::OPENING:
+            drawFrame(width, height, buffer, "opening");
+            break;
+        case EyesState::BLINKING:
+            drawFrame(width, height, buffer, "blinking");
+            break;
+        case EyesState::THINKING:
+            drawFrame(width, height, buffer, "thinking");
+            break;
+        case EyesState::SPEAKING:
+            drawFrame(width, height, buffer, "speaking");
+            break;
+        case EyesState::SMILING:
+            drawFrame(width, height, buffer, "smiling");
+            break;
+        case EyesState::DVD:
+            drawFrame(width, height, buffer, "dvd");
+            break;
+        case EyesState::JUMPING:
+            drawFrame(width, height, buffer, "jumping");
+            break;
+        case EyesState::CLOSING:
+            drawFrame(width, height, buffer, "closing");
+            break;
+        default:
+            assert(false);
+            // drawDefault(width, height, buffer);
+            return;
+    }
+}
+
+void EyesView::draw(int width, int height, uint16_t* buffer) {
+    if (frameIndex == 0) {
+        onAnimationStartCallback();
+    }
+    
+    drawFrame(state, width, height, buffer);
+
+    if (frameIndex == animationFrames(state) - 1) {
+        onAnimationEndCallback();
     } else {
         frameIndex += 1;
     }
 }
 
-void EyesView::draw(int width, int height, uint16_t* buffer) {
-    switch (state) {
-        case EyesState::NONE:
-            return;
-        case EyesState::OPENING:
-            drawFrame(width, height, buffer, "opening", 362);
-            break;
-        case EyesState::BLINKING:
-            drawFrame(width, height, buffer, "blinking", 363);
-            break;
-        case EyesState::THINKING:
-            drawFrame(width, height, buffer, "thinking", 398);
-            break;
-        case EyesState::SPEAKING:
-            drawFrame(width, height, buffer, "speaking", 333);
-            break;
-        case EyesState::SMILING:
-            drawFrame(width, height, buffer, "smiling", 333);
-            break;
-        case EyesState::DVD:
-            drawFrame(width, height, buffer, "dvd", 363);
-            break;
-        case EyesState::JUMPING:
-            drawFrame(width, height, buffer, "jumping", 363);
-            break;
-        case EyesState::CLOSING:
-            drawFrame(width, height, buffer, "closing", 363);
-            break;
-        default:
-            assert(false);
-            // drawDefault(width, height, buffer);
-            break;
-    }
+// Assert NONE state
+void EyesView::updateState(EyesState state) {
+    frameIndex = 0;
+    this->state = state;
 }
